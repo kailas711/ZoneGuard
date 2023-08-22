0d42676e8af3ba229c50ff1a2ce8f71068001255
Kailas P Sudheer <89206677+kailas711@users.noreply.github.com>
2023-08-22 22:03:01 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Boundary_Area_Mapping
-A Django based MUP uses object detection technique to assist and enhance monitoring  
+# Django-registration-and-login-system
+This web app has been developed as part of my group project in Forge , using the popular Django framework and JavaScript for the frontend. This mini-app can be easily integrated into a bigger system project that needs to have a registration and login system and that need AI surveilence.
+
+### Basic Features of The App
+    
+* Register – Users can register and create a new profile
+
+* Login - Registered users can login using username and password
+
+* Social Apps Login – Users can login using their GitHub or Google account
+
+* User Profile - Once logged in, users can create and update additional information such as avatar and bio in the profile page
+
+* Update Profile – Users can update their information such as username, email, password, avatar and bio
+
+* Remember me – Cookie Option, users don’t have to provide credentials every time they hit the site
+
+* Forgot Password – Users can easily retrieve their password if they forget it 
+* Admin Panel – admin can CRUD users
+
+* Integrate CCTV/IP stream for AI based object detection and alarm for industrial saftey 
+
+### Quick Start
+To get this project up and running locally on your computer follow the following steps.
+1. Set up a python virtual environment
+2. Run the following commands
+```
+$ pip install -r requirements.txt
+$ python manage.py migrate
+$ python manage.py createsuperuser
+$ python manage.py runserver
+```
+   
+3. Open a browser and go to http://localhost:8000/
+
---
+++ users/views.py
@@ -0,0 +1,214 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordChangeView
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views import View
+from django.contrib.auth.decorators import login_required
+
+from .forms import RegisterForm, LoginForm, UpdateUserForm, UpdateProfileForm
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import cv2
+import numpy as np
+from PIL import Image
+import torch
+import json
+from datetime import datetime
+
+# Load YOLOv5 model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5m', pretrained=True)
+model.conf = 0.5
+
+def home(request):
+    return render(request, 'users/home.html')
+
+
+class RegisterView(View):
+    form_class = RegisterForm
+    initial = {'key': 'value'}
+    template_name = 'users/register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        # will redirect to the home page if a user tries to access the register page while logged in
+        if request.user.is_authenticated:
+            return redirect(to='/')
+
+        # else process dispatch as it otherwise normally would
+        return super(RegisterView, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+
+            return redirect(to='login')
+
+        return render(request, self.template_name, {'form': form})
+
+
+# Class based view that extends from the built in login view to add a remember me functionality
+class CustomLoginView(LoginView):
+    form_class = LoginForm
+
+    def form_valid(self, form):
+        remember_me = form.cleaned_data.get('remember_me')
+
+        if not remember_me:
+            # set session expiry to 0 seconds. So it will automatically close the session after the browser is closed.
+            self.request.session.set_expiry(0)
+
+            # Set session as modified to force data updates/cookie to be saved.
+            self.request.session.modified = True
+
+        # else browser session will be as long as the session cookie time "SESSION_COOKIE_AGE" defined in settings.py
+        return super(CustomLoginView, self).form_valid(form)
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    subject_template_name = 'users/password_reset_subject'
+    success_message = "We've emailed you instructions for setting your password, " \
+                      "if an account exists with the email you entered. You should receive them shortly." \
+                      " If you don't receive an email, " \
+                      "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = reverse_lazy('users-home')
+
+
+class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
+    template_name = 'users/change_password.html'
+    success_message = "Successfully Changed Your Password"
+    success_url = reverse_lazy('users-home')
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect(to='users-profile')
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+
+    return render(request, 'users/profile.html', {'user_form': user_form, 'profile_form': profile_form})
+
+@login_required
+
+@csrf_exempt
+def livestream(request):
+    if request.method == 'POST':
+        # Get user-drawn bounding box coordinates, IP address, and screen name from the request
+        bounding_boxes_str = request.POST.get('boxes')
+        ip = request.POST['ip']
+        screen = request.POST['screen']
+        print("Screen: ", screen)
+        print(bounding_boxes_str)
+        
+        # Convert the bounding_boxes string to a dictionary
+        bounding_boxes = json.loads(bounding_boxes_str)
+
+        # Initialize a list to store the detection results
+        detection_results = []
+
+        # Iterate through bounding box coordinates
+        for box in bounding_boxes:
+            # Assign x1, y1, x2, y2 from the current bounding box coordinate
+            x1, y1, x2, y2 = box
+
+            # Open the video stream
+            video_path = f"http://{ip}"
+            detected_list = []
+            box_list = []
+            cap = cv2.VideoCapture(video_path)
+            if not cap.isOpened():
+                return render(request, 'livestream.html', {'error': 'Error opening video stream!'})
+
+            # Initialize detection flag
+            detected = False
+
+            # Read the frame from the video stream
+            ret, frame = cap.read()
+
+            # Check if a frame is read successfully
+            if not ret:
+                break
+
+            # Crop the frame to the selected bounding box
+            danger_zone = frame[y1:y2, x1:x2]
+
+            # Convert the cropped frame to PIL Image
+            pil_image = Image.fromarray(danger_zone)
+
+            # Perform object detection using YOLOv5
+            results = model(pil_image)
+            labels = results.xyxyn[0].cpu().numpy()[:, -1]
+            classes = model.names[int(labels[0])] if len(labels) > 0 else None
+
+            # Check if a person is detected in the danger zone
+            if classes == 'person':
+                detected = True
+                detected_list.append(detected)
+                box_list.append(box)
+                print("TRUE ===", box, "IP ===", ip)
+                log_detection(ip, screen)  # Call the log_detection() function to store the detection information
+            else:
+                detected_list.append(detected)
+                box_list.append(box)
+                print("FALSE ===", box, "IP ===", ip)
+
+            # Add the detection result to the list
+            detection_results.append({'detected': detected, 'box': box})
+            print(detection_results)
+            print(type(detection_results))
+
+            cap.release()
+
+        results = [{'detected': detected, 'box': box, 'ip': ip} for detected, box in zip(detected_list, box_list)]
+        # Return the detection results as JSON response
+        return JsonResponse({'results': detection_results})
+
+    return render(request, 'users/livestream.html')
+
+
+def log_detection(ip, screen):
+    current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_entry = {
+        "timestamp": current_datetime,
+        "Person Detected":True,
+        "ip_address": ip,
+        "screen": screen
+    }
+
+ 
+    log_file_path = "log_file.json"  
+
+    # Load existing log entries from the file
+    existing_logs = []
+    try:
+        with open(log_file_path, "r") as log_file:
+            existing_logs = json.load(log_file)
+    except FileNotFoundError:
+        pass
+
+    # Append the new log entry to the existing logs
+    existing_logs.append(log_entry)
+
+    # Write the updated log entries to the file
+    with open(log_file_path, "w") as log_file:
+        json.dump(existing_logs, log_file)
